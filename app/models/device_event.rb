30f1904855f31b7eecf96e6af722581c71996ac4
nishan.mahamalimage2 <nishan.mahamalimage2@sky.uk>
2023-12-12 15:24:43 +0000

    feat(create_event): added device event model validation

--- app/models/device_event.rb
+++ app/models/device_event.rb
@@ -1,3 +1,21 @@
 class DeviceEvent < ApplicationRecord
+    before_create :set_received_at
 
+    validates :category, :recorded_at, presence: true
+    attr_readonly :received_at
+    validate :attributes_not_set_at_creation
+
+    def set_received_at
+        self.received_at = Time.current
+    end
+
+    private 
+
+    def attributes_not_set_at_creation
+        if new_record?
+            if notification_sent.present? || is_deleted.present? || received_at.present?
+                errors.add(:base, "notification_sent, is_deleted or received_at cannot be set at creation")
+            end
+        end
+    end
 end
--- test/models/device_event_test.rb
+++ test/models/device_event_test.rb
@@ -10,7 +10,6 @@ class DeviceEventTest < ActiveSupport::TestCase
 
   test 'invalid device event missing recorded_at' do
     device_event = DeviceEvent.new(device_uuid: "342354764723", category: 2)
-    puts device_event.received_at
     assert_not device_event.valid?
   end
 
@@ -20,17 +19,17 @@ class DeviceEventTest < ActiveSupport::TestCase
     assert_not device_event.valid?
   end
 
-  test 'invalid device event with received_at' do
+  test 'invalid device event with received_at set at creation' do
     device_event = DeviceEvent.new(recorded_at: Date.today, received_at: Date.today, device_uuid: "342354764723", category: 2)
     assert_not device_event.valid?
   end
 
-  test 'invalid device event with notification_sent' do
+  test 'invalid device event with notification_sent set at creation' do
     device_event = DeviceEvent.new(recorded_at: Date.today, received_at: Date.today, device_uuid: "342354764723", notification_sent: false, category: 2)
     assert_not device_event.valid?
   end
 
-  test 'invalid device event with is_deleted' do
+  test 'invalid device event with is_deleted set at creation' do
     device_event = DeviceEvent.new(recorded_at: Date.today, received_at: Date.today, device_uuid: "342354764723", is_deleted: false, category: 2)
     assert_not device_event.valid?
   end
