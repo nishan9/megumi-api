326c7343189863741c332b5c50027de83c3fbe10
nishan.mahamalimage2 <nishan.mahamalimage2@sky.uk>
2023-12-13 12:24:36 +0000

    test(create_event): added device event controller tests

--- test/controllers/device_events_controller_test.rb
+++ test/controllers/device_events_controller_test.rb
@@ -1,7 +1,29 @@
 require "test_helper"
 
 class DeviceEventsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "create valid device event with valid parameters" do
+    post '/api/v1/device_events', params: { device_event: { category: 1, recorded_at: Date.current } }
+    assert_response :ok
+    device_event = JSON.parse(response.body)
+    assert_not_nil device_event['uuid']
+    assert_equal 1, device_event['category']
+  end
+
+  test "create invalid device event with category parameter missing" do
+    post '/api/v1/device_events', params: { device_event: { recorded_at: Date.current } }
+    assert_response :unprocessable_entity
+    errors = JSON.parse(response.body)
+    expected_error_message = { "category" => ["can't be blank"] }
+    assert_equal expected_error_message, errors
+  end
+
+  test "create invalid device event with recorded_at parameter missing" do
+    post '/api/v1/device_events', params: { device_event: { category: 1 } }
+    assert_response :unprocessable_entity
+    errors = JSON.parse(response.body)
+    expected_error_message = { "recorded_at" => ["can't be blank"] }
+    assert_equal expected_error_message, errors
+  end
+
 end
