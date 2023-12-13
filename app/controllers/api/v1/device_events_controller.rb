531e0a6e0c05a9a715636eac47e3166ce89678c7
nishan.mahamalimage2 <nishan.mahamalimage2@sky.uk>
2023-12-13 12:35:08 +0000

    feat(create_event): created device entity endpoint

--- app/controllers/api/v1/device_events_controller.rb
+++ app/controllers/api/v1/device_events_controller.rb
@@ -1,4 +1,14 @@
 class Api::V1::DeviceEventsController < ApplicationController
-    def create
+    def create 
+        device_event = DeviceEvent.new(device_event_params)
+        if device_event.save
+            render json: device_event, status: :ok
+        else 
+            render json: device_event.errors, status: :unprocessable_entity
+        end 
+    end
+
+    def device_event_params
+        params.require(:device_event).permit(:category, :recorded_at)
     end
 end
